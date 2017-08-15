c071d189c92633987aa59093eb664d71859d1ffc
Robert Turner Jr <georob23@gmail.com>
2017-08-14 18:27:13 -0700

    Add reading binary file code, binary file examples (built with c++).

--- spike/writeFiles.js
+++ spike/writeFiles.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 const crypto = require('crypto');  // not needed for files, just for bin example
 
-// Write some UTF8 encoded stuff (UTF8 is the default)
-let utf8stream = fs.createWriteStream('utf8.txt');
-utf8stream.on('finish', () => {
-  console.log('utf8.txt has been written\n')
-});
-utf8stream.write('UTF-8 is really, really great!\n');
-utf8stream.write('That\'s why it is the default.\n' );
-utf8stream.end();
-
-// Write some UTF16 encoded stuff
-let utf16stream = fs.createWriteStream('utf16.txt');
-utf16stream.on('finish', () => {
-  console.log('utf16.txt has been written\n')
-});
-utf16stream.write('UTF-16 is wide chars?\n');
-utf16stream.write('We\'ll know soon!\n' );
-utf16stream.end();
+// // Write some UTF8 encoded stuff (UTF8 is the default)
+// let utf8stream = fs.createWriteStream('utf8.txt');
+// utf8stream.on('finish', () => {
+//   console.log('utf8.txt has been written\n')
+// });
+// utf8stream.write('UTF-8 is really, really great!\n');
+// utf8stream.write('That\'s why it is the default.\n' );
+// utf8stream.end();
+//
+// // Write some UTF16 encoded stuff
+// let utf16stream = fs.createWriteStream('utf16.txt');
+// utf16stream.on('finish', () => {
+//   console.log('utf16.txt has been written\n')
+// });
+// utf16stream.write('UTF-16 is wide chars?\n');
+// utf16stream.write('We\'ll know soon!\n' );
+// utf16stream.end();
 
 // Try to write some binary
 let binstream = fs.createWriteStream('best.bin');
@@ -28,5 +28,8 @@ binstream.on('finish', () => {
 let buffer = crypto.randomBytes(100);
 binstream.write(buffer);
 // Another way
-binstream.write(crypto.randomBytes(100));
+let binwords = Buffer.from("There were actually 42 sea turtles!!");
+console.log("Turtles:\n");
+console.log(binwords);
+binstream.write(binwords);
 binstream.end();
