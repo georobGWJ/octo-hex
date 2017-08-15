e6a4b54d4dcb97e49d00655af2ff9a872ee38331
Robert Turner Jr <georob23@gmail.com>
2017-08-14 18:51:45 -0700

    Add top buttons.

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ let mainWindow = null;
 
 app.on('ready', () => {
   console.log('Electron is ready...');
+  mainWindow = new BrowserWindow();
   mainWindow.loadURL(`file://${__dirname}/index.html`);   // Create and populate the window
 
   mainWindow.on('closed', () => {
