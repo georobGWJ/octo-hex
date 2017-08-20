636987081852e4e1f09511c6fecb45f491996782
Robert Turner Jr <georob23@gmail.com>
2017-08-19 20:00:31 -0700

    Add open file dialog upon start.

--- app/main.js
+++ app/main.js
@@ -4,10 +4,25 @@ let mainWindow = null;
 
 app.on('ready', () => {
   // console.log('Electron is ready...');
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({ show: false });
   mainWindow.loadURL(`file://${__dirname}/index.html`);   // Create and populate the window
+
+  const getFileFromUser = () => {
+    const files = dialog.showOpenDialog({                 // Trigger file open dialog
+      properties: ['openFile']                            // configuration object
+    });
+    if (!files) { return; }                               // return if no files
+
+    console.log(files);                                   // Log files to console
+  }
+
 
   mainWindow.on('closed', () => {
     mainWindow = null;
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    getFileFromUser();
   });
 });
